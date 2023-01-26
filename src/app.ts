9a7fa4059a474d53ace74eca5403a2f7deac576d
Danielle-Luz <daniln2209@gmail.com>
2023-01-25 22:58:32 -0300

    feat: add get all lists function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import { createList } from './callbacks/index';
+import { createList, getAllLists } from './callbacks/index';
 const express = require("express");
 const api = express();
 
 api.use(express.json());
 
 api.post("/purchaseList", createList);
+
+api.get("/purchaseList", getAllLists);
 
 api.listen(3000, () => console.log("API is running"));
--- src/callbacks/index.ts
+++ src/callbacks/index.ts
@@ -25,3 +25,7 @@ export const createList = (request: Request, response: Response) => {
     return response.status(400).send(errorMessage);
   }
 };
+
+export const getAllLists = (request: Request, response: Response) => {
+    return response.status(200).send(database);
+}
