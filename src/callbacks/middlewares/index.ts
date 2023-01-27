7496665a8c89f18de78fcce4588a1509eb8ff792
Danielle-Luz <daniln2209@gmail.com>
2023-01-27 00:13:38 -0300

    feat: add middlewares namespace

--- src/callbacks/middlewares/index.ts
+++ src/callbacks/middlewares/index.ts
@@ -2,46 +2,48 @@ import { iMessage } from "./../../interfaces";
 import { NextFunction, Request, Response } from "express";
 import { database } from "../../database";
 
-export const validateId = (
-  request: Request,
-  response: Response,
-  next: NextFunction
-) => {
-  const id = Number(request.params["purchaseListId"]);
+export namespace middlewares {
+  export const validateId = (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    const id = Number(request.params["purchaseListId"]);
 
-  const idIsNotANumber = isNaN(id);
-  const idIsDecimal = id % 1 !== 0;
-  const idIsNegative = id <= 0;
+    const idIsNotANumber = isNaN(id);
+    const idIsDecimal = id % 1 !== 0;
+    const idIsNegative = id <= 0;
 
-  if (idIsNotANumber || idIsDecimal || idIsNegative) {
-    const errorMessage: iMessage = {
-      message: "O id deve ser um número inteiro positivo",
-    };
+    if (idIsNotANumber || idIsDecimal || idIsNegative) {
+      const errorMessage: iMessage = {
+        message: "O id deve ser um número inteiro positivo",
+      };
 
-    return response.status(400).send(errorMessage);
-  }
+      return response.status(400).send(errorMessage);
+    }
 
-  return next();
-};
+    return next();
+  };
 
-export const findList = (
-  request: Request,
-  response: Response,
-  next: NextFunction
-) => {
-  const searchedListId = Number(request.params["purchaseListId"]);
+  export const findList = (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    const searchedListId = Number(request.params["purchaseListId"]);
 
-  const foundList = database.find(
-    ({ id: listId }) => listId === searchedListId
-  );
+    const foundList = database.find(
+      ({ id: listId }) => listId === searchedListId
+    );
 
-  if (!foundList) {
-    const errorMessage: iMessage = {
-      message: "Nenhuma lista possui o id especificado",
-    };
+    if (!foundList) {
+      const errorMessage: iMessage = {
+        message: "Nenhuma lista possui o id especificado",
+      };
 
-    return response.status(404).send(errorMessage);
-  }
+      return response.status(404).send(errorMessage);
+    }
 
-  return next();
-};
+    return next();
+  };
+}
